e2e0754b78e2e3308198ed79a648958d6eb592a0
smart40k <smartdaction@gmail>
2022-09-13 13:54:46 -0700

    i love you

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,12 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
- * */
+ * main - Entry point
+ * Code description
+ * Return: Always 0
+ */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	printf("_putchar\n");
 	return (0);
 }
