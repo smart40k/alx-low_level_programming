d599e367e15c34d422848836645af29e20693148
smart40k <smartdaction@gmail>
2022-09-19 12:25:05 -0700

    peter obi

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,33 +1,18 @@
 #include "main.h"
-
 /**
- * puts_half - prints half of a string
- * followed by a new line
- * @str: string to be printed
+ * puts_half - function
+ *
+ * @str: pointer of parameter
  */
 void puts_half(char *str)
 {
-	int len, n, i;
+	int length = 0;
+	int sublength;
 
-	len = 0;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-
-	if (len % 2 == 0)
-	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	} else if (len % 2)
-	{
-		for (n = (len - 1) / 2; n < len - 1; n++)
-		{
-			_putchar(str[n + 1]);
-		}
-	}
-	_putchar('\n');
+	while (str[length] != '\0')
+		length++;
+	sublength = (length % 2 == 0) ? length / 2 : (length + 1) / 2;
+	while (str[sublength] != '\0')
+		_putchar(str[sublength++]);
+	_putchar(10);
 }
