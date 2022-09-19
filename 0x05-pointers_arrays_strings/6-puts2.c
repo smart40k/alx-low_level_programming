23eace74b9589bfa75028bfc88298cbada4b1eb4
smart40k <smartdaction@gmail>
2022-09-19 10:10:17 -0700

    peter obi

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,27 +1,25 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * puts2 - print  every other character of a string
- * @str:pointer char
- * return:void
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
  */
 void puts2(char *str)
 {
-	int i, x;
+	int len, i;
 
-	i = 0;
+	len = 0;
 
-	x = 0;
-
-	while (str[i++])
+	while (str[len] != '\0')
 	{
-		x++;
+		len++;
 	}
 
-	for (i = 0; i < x; i += 2)
+	for (i = 0; i < len; i += 2)
 	{
-		_putchar(*(str + i));
+		_putchar(str[i]);
 	}
-	
+
 	_putchar('\n');
 }
