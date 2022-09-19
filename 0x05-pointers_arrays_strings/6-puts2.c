23e5b1ad05969a6ca53efe1969d5537a1574d689
smart40k <smartdaction@gmail>
2022-09-19 11:36:52 -0700

    peter obi

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,20 @@
 void puts2(char *str)
 {
 	int i, x;
+
 	i = 0;
+
 	x = 0;
+
 	while (str[i++])
 	{
 		x++;
 	}
+
 	for (i = 0; i < x; i += 2)
 	{
 		_putchar(*(str + i));
 	}
+
 	_putchar('\n');
 }
