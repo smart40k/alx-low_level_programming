6bf5da68f81a2fbd1133c60837d9a678c7f2ca4e
smart40k <smartdaction@gmail>
2022-09-22 16:55:51 -0700

    peter obi

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- *  @r: buffer for result
- *  @size_r: buffer size
- *
- *  Return: address of r or 0
+ * infinite_add - adds two numbers.
+ * @n1:char
+ * @n2:char.
+ * @r:char
+ * @size_r:int
+ * Return:char.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
+
 	for (i = 0; n1[i]; i++)
 		;
 	for (j = 0; n2[j]; j++)
