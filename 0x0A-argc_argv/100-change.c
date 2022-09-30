c3441a53cc92749cbea965aba25020972da7a9cd
smart40k <smartdaction@gmail>
2022-09-30 14:04:24 -0700

    life

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
+
 /**
- * main - adds positive numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - print the minimum number of coins to 
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * 
+ *  Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	for (i = 1; i < argc; i++)
+	if (argc != 2)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		add += atoi(argv[i]);
+		printf("Error\n");
+		return (1);
 	}
-	printf("%d\n", add);
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+
+	printf("%d\n", result);
 	return (0);
 }
