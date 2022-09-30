23210ca84497424f52bd04e42cec337923e808c6
smart40k <smartdaction@gmail>
2022-09-30 09:01:08 -0700

    tony

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - mul 2 argv
- *  @argc:int
- *  @argv:string
- *  Return:int
+ * main - multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ *  Return: 0 - success
  */
+
 int main(int argc, char *argv[])
 {
-	int x, i, j;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		i = atoi(argv[1]);
-		j = atoi(argv[2]);
-		x = i *j;
-		printf("%d\n", x);
-	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
