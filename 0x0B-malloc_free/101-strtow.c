aea286322ec9fc996053336631b04dca42f5c7be
smart40k <smartdaction@gmail>
2022-10-03 10:53:28 -0700

    love

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -39,7 +39,7 @@ char **strtow(char *str)
 	n = wrdcnt(str);
 	if (n == 1)
 		return (NULL);
-	x = (char **)malloc(n *sizeof(char *));
+	x = (char **)malloc(n * sizeof(char *));
 	if (x == NULL)
 		return (NULL);
 	x[n - 1] = NULL;
@@ -51,7 +51,7 @@ char **strtow(char *str)
 			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 				;
 			j++;
-			x[ch] = (char *)malloc(j *sizeof(char));
+			x[ch] = (char *)malloc(j * sizeof(char));
 			j--;
 			if (x[ch] == NULL)
 			{
