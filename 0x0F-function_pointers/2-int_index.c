823aa7a44adb00ce22c815ea2bb06444673bec60
smart40k <smartdaction@gmail>
2022-10-12 00:47:27 -0700

    life

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 /**
- * array_iterator - excute a function on array element
- * @array: array
- * @size: size_t
- * @action:function to be excuted
- * Return:void
+ *  array_iterator - excute a function on array element
+ *  @array: array
+ *  @size: size_t
+ *  @action:function to be excuted
+ *  Return:void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
