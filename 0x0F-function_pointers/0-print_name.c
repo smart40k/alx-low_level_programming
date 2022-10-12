59b1bf61b716d1372681c622c99295092e8a9495
smart40k <smartdaction@gmail>
2022-10-12 11:12:01 -0700

    life

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,12 @@
 #include "function_pointers.h"
-#include <stdio.h>
-
 /**
- * print_name - Prints a name
- * @name: string
- * @f: function
- *
- * Return: void
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: name is printed according to this function
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name && f)
 		f(name);
 }
